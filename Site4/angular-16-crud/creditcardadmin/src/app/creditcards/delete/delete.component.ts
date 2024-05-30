7384a60159977eba6b43a511d8c364b5465b0f68
kerdo1 <kerdovahk@gmail.com>
2024-05-30 14:33:50 +0300

    Update

--- Site4/angular-16-crud/creditcardadmin/src/app/creditcards/delete/delete.component.ts
+++ Site4/angular-16-crud/creditcardadmin/src/app/creditcards/delete/delete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-delete',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete.component.scss']
 })
 export class DeleteComponent {
+
+  creditcardId!: Number;
+
+  constructor(private router: ActivatedRoute,
+    private route: Router,
+    private creditcardsService: CreditcardsService) {
+    this.creditcardId = parseInt(this.router.snapshot.paramMap.get("id") || '');
+
+    //del function
+    this.creditcardsService.deleteCreditCard(this.creditcardId).subscribe(data => {
+      alert("Deleted the credit card");
+      this.route.navigate(['/creditcards'])
+    })
+  }
 
 }
--- Site4/angular-16-crud/creditcardadmin/src/app/creditcards/view/view.component.ts
+++ Site4/angular-16-crud/creditcardadmin/src/app/creditcards/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
 import { CreditcardsService } from 'src/app/services/creditcards.service';
 
@@ -10,9 +11,13 @@ import { CreditcardsService } from 'src/app/services/creditcards.service';
 export class ViewComponent {
 
   creditCardDetails!: CreditCard;
+  creditCardId: Number;
 
-  constructor(private creditCardsService: CreditcardsService){
-    this.creditCardsService.getCreditCardById(3).subscribe((data: CreditCard) => {
+  constructor(private creditCardsService: CreditcardsService,
+    private router: ActivatedRoute){
+    this.creditCardId = parseInt(this.router.snapshot.paramMap.get("id") || '');
+
+    this.creditCardsService.getCreditCardById(this.creditCardId).subscribe((data: CreditCard) => {
       this.creditCardDetails = data
     })
   

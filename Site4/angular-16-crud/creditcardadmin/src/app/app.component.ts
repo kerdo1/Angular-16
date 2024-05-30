73c57454bc4235c700805ea9ca157c18963382cd
kerdo1 <kerdovahk@gmail.com>
2024-05-30 11:33:11 +0300

    Update

---
+++ Site4/angular-16-crud/creditcardadmin/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'creditcardadmin';
+  sidebarOpen = true;
+  toggleSidebar(){
+  this.sidebarOpen = this.sidebarOpen? false: true;
+  }
+}

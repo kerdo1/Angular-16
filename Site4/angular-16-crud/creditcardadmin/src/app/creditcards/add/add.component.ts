d06871dde411a726f12c0b9e3a9f383bb1ed064b
kerdo1 <kerdovahk@gmail.com>
2024-06-02 17:20:44 +0300

    Update

--- Site4/angular-16-crud/creditcardadmin/src/app/creditcards/add/add.component.ts
+++ Site4/angular-16-crud/creditcardadmin/src/app/creditcards/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
-
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
@@ -8,6 +9,10 @@ import { CreditCard } from 'src/app/models/credit-card';
 })
 export class AddComponent {
 
+  constructor(private creditcardsService: CreditcardsService,
+    private router: Router) {
+    
+  }
   newCreditCard: CreditCard = {
       id: undefined,
       name: "",
@@ -24,6 +29,10 @@ export class AddComponent {
   }
 
   saveCreditCard(){
-    console.log("Form Submitted")
-  }
+    console.log("Form Submitted");
+    this.creditcardsService.createCreditCard(this.newCreditCard).subscribe(data => {alert("Credit Card Added");
+    this.router.navigate(['creditcards']);
+  })
+}
+
 }
